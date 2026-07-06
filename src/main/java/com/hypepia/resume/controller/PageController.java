370056b7ea5ab8d00e533932ff249b611fe72780
Q219057 <Q219057@DESKTOP-LLJDU3K>
2026-07-06 16:15:01 +0900

    자소서 탭 추가 및 포폴 추가

--- src/main/java/com/hypepia/resume/controller/PageController.java
+++ src/main/java/com/hypepia/resume/controller/PageController.java
@@ -1,21 +1,33 @@
 package com.hypepia.resume.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
     @GetMapping("/")
-    public String resume(Model model) {
-        model.addAttribute("activePage", "resume");
+    public String resume() {
         return "resume";
     }
 
+    @GetMapping("/introduction")
+    public String introduction() {
+        return "introduction";
+    }
+
     @GetMapping("/portfolio")
-    public String portfolio(Model model) {
-        model.addAttribute("activePage", "portfolio");
+    public String portfolio() {
         return "portfolio";
+    }
+
+    @GetMapping("/portfolio/stocksense")
+    public String stocksense() {
+        return "portfolio-stocksense";
+    }
+
+    @GetMapping("/portfolio/apiverse")
+    public String apiverse() {
+        return "portfolio-apiverse";
     }
 }
